01618adb21d81d660566733df2128f5d06306bfd
Michael Kelly <mike@whatsthatweb.com.au>
2017-07-07 19:09:38 +1000

    Removing firebase

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -74,15 +74,4 @@
 
   // Your custom JavaScript goes here
 
-  // Firebase
-  var config = {
-    apiKey: "AIzaSyD7EolC5c-dPJfjREPJs4nenbFKthU8Ogg",
-    authDomain: "apf-portal.firebaseapp.com",
-    databaseURL: "https://apf-portal.firebaseio.com",
-    projectId: "apf-portal",
-    storageBucket: "",
-    messagingSenderId: "412696605647"
-  };
-  firebase.initializeApp(config);
-
 })();
