169dbb1b1a37a6206772edef6a6e8841d86c3179
Michael Kelly <mike@whatsthatweb.com.au>
2017-07-09 09:34:12 +1000

    Adding navbar, and fixing images.

---
+++ app/scripts/layout/directives/navbar.directive.js
@@ -0,0 +1,38 @@
+/* eslint no-unused-vars: 0 */
+/* eslint require-jsdoc: 0 */
+/* global angular */
+(function() {
+  'use strict';
+
+  angular
+    .module('app.layout')
+    .directive('gzNavbar', gzNavbar);
+
+  gzNavbar.$inject = [
+    'layoutService'
+  ];
+
+  function gzNavbar(layoutService) {
+    return {
+      templateUrl: 'scripts/layout/directives/navbar.template.html',
+      restrict: 'E',
+      controller: NavbarController,
+      controllerAs: 'vm',
+      link: function(scope) {
+        scope.player = layoutService.player;
+      }
+    };
+  }
+
+  NavbarController.$inject = [
+    'layoutService'
+  ];
+
+  function NavbarController(
+    layoutService
+  ) {
+    var vm = this;
+    
+    vm.navigate = layoutService.navigate;
+  }
+})();
