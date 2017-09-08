a46f3c40997b25df5f4b554205a37cd28fcda6ac
Michael Kelly <mike@whatsthatweb.com.au>
2017-09-08 17:06:54 +1000

    Pointing to wrong server. I need local and production stuff installed in this dammit.

--- app/scripts/core/constants.js
+++ app/scripts/core/constants.js
@@ -6,6 +6,6 @@
   angular
     .module('app.core')
     // .constant('API_URI', ['https://api.asiapacificforum.net/salesforce']);
-    // .constant('API_URI', ['https://apfapi.herokuapp.com/salesforce']);
-    .constant('API_URI', ['http://localhost:7000/salesforce']);
+    .constant('API_URI', ['https://apfapi.herokuapp.com/salesforce']);
+    // .constant('API_URI', ['http://localhost:7000/salesforce']);
 })();
